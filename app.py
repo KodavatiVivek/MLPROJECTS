2d496074dc26f6ac9c5cc200e1a5be66d8f6b329
Kodavati/vivek <vivekchowdary457@gmail.com>
2025-06-05 14:46:26 +0530

    Deplpy

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+
+#utils
+from src.pipeline.Predict_pipeline import CustomData, PredictPipeline
+
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.preprocessing import StandardScaler
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template ('predict.html')
+        # Get the input values from the form
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+        logging.info("Data received from the form")
+        pred_df=data.get_data_as_df()
+        logging.info(f"DataFrame created: {pred_df}")
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict_data(pred_df)
+        logging.info(f"Prediction results: {results}")
+
+        return render_template('predict.html', results=results[0])
+
+
+if __name__=="__main__":
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)
+    print("Starting the application...")  # Debugging line to confirm app start
+    logging.info("Application started successfully")  # Log the start of the application
+    print(f"link: http://localhost:8080")  # Debugging line to confirm the link
+    logging.info("Application is running on http://localhost:8080")  # Log the link
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,19 @@ from src.exception import CustomException
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import GridSearchCV
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            obj = pickle.load(file)
+        logging.info(f"Object loaded from {file_path}")
+        return obj
+    except Exception as e:
+        logging.error(f"Error loading object from {file_path}: {str(e)}")
+        raise CustomException(e, sys)
+    
+    
 
 def save_object(file_path, obj):
     """
@@ -19,7 +32,7 @@ def save_object(file_path, obj):
         logging.error(f"Error saving object: {str(e)}")
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     """
     Evaluate multiple regression models and return a report of their performance.
     """
@@ -27,9 +40,23 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         model_report = {}
         for i in range(len(list(models))):
             model_name= list(models.keys())[i]
-            model = models[model_name]
-            #Fir the model on train data
-            model.fit(X_train, y_train)
+            logging.info(f"Evaluating model: {model_name}")
+            #Get the model
+            if model_name in params:
+                para = params[model_name]
+                model = models[model_name]
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, verbose=2)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+                logging.info(f"Best parameters for {model_name}: {gs.best_params_}")
+            else:
+                logging.info(f"No specific parameters for {model_name}, using default settings.")
+                model = models[model_name]
+                model.fit(X_train, y_train)
+            #Log the best parameters found by GridSearchCV
+            
+
             #Predict on train data
             y_train_pred = model.predict(X_train)
             #Predict on test data
@@ -45,7 +72,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
                 'mean_squared_error': mse
             }
 
-            return model_report
+        return model_report
         # Sort the model report based on r2_score
     except Exception as e:
         logging.error(f"Error evaluating models: {str(e)}")
